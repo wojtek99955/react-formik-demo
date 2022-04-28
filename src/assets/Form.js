09ba44037b7d2b02a8cb6deb601ba5875d9ab933
Wojtek Książek <wojtek99955@gmail.com>
2022-04-28 12:38:58 +0200

    Form styles update

--- src/assets/Form.js
+++ src/assets/Form.js
@@ -4,19 +4,41 @@ import { User } from "@styled-icons/boxicons-solid/User";
 
 const Main = styled.main`
   background: white;
-  padding: 2rem;
+  padding: 2.5rem;
   position: relative;
+  width: 400px;
+  border-radius: 10px;
+  -webkit-box-shadow: 2px 3px 39px -11px rgba(66, 68, 90, 0.75);
+  -moz-box-shadow: 2px 3px 39px -11px rgba(66, 68, 90, 0.75);
+  box-shadow: 2px 3px 39px -11px rgba(66, 68, 90, 0.75);
 `;
 const StyledForm = styled.form`
   label {
     display: block;
+    color: #b6d7d0;
   }
   button {
     display: block;
+    border: none;
+    color: white;
+    margin-top: 1rem;
+    padding: 0.7rem 1.2rem;
+    cursor: pointer;
+    border-radius: 15px;
+    background: rgb(151, 122, 255);
+    background: linear-gradient(
+      90deg,
+      rgba(151, 122, 255, 1) 19%,
+      rgba(191, 123, 249, 1) 100%
+    );
   }
   input {
     background-color: #ebf4f2;
     border: none;
+    border-radius: 5px;
+    margin: 0.5rem 0;
+    padding: 0.5rem 0.2rem;
+    width: 100%;
   }
 `;
 const UserContainer = styled.div`
